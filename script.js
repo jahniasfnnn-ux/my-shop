3da0c011c96fc9bb3ed69013d0b395b72d77681c
jahniasfnnn-ux <jahniasfnnn@gmail.com>
2026-05-19 23:32:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+        const shopBtn = document.querySelector('.btn');
+            if (shopBtn) {
+                    shopBtn.addEventListener('click', () => {
+                                alert('مرحباً بك في WD BEIN! تم تفعيل زر التسوق بنجاح.');
+                                        });
+                                            }
+                                            });
+                                            
+})
