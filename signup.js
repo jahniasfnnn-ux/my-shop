4d2a2cd97a1ffcfad412fe710e0469732b4e00de
jahniasfnnn-ux <jahniasfnnn@gmail.com>
2026-05-21 16:57:56 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,18 +1,16 @@
-import { auth } from './auth.js'; // نستورد خاصية الـ auth التي جهزناها سابقاً
+import { auth } from './auth.js'; 
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
 const signupBtn = document.getElementById('signupBtn');
 
 signupBtn.addEventListener('click', () => {
-    const email = emailInput.value;
-    const password = passwordInput.value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            alert("تم إنشاء الحساب بنجاح!");
-            console.log("المستخدم:", userCredential.user);
+            alert("تم إنشاء الحساب بنجاح! أهلاً بك.");
+            console.log("المستخدم الجديد:", userCredential.user);
         })
         .catch((error) => {
             alert("خطأ: " + error.message);
