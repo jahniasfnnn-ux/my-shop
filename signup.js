5becb116b320f610628c65e0ea5d1ba1fda732c3
jahniasfnnn-ux <jahniasfnnn@gmail.com>
2026-05-21 19:58:21 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,16 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { auth } from "./auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBPrIbAxrLNUPINKa4nf0MVM1_Jf32vb1A",
-  authDomain: "myshop-9cf11.firebaseapp.com",
-  projectId: "myshop-9cf11",
-  storageBucket: "myshop-9cf11.firebasestorage.app",
-  messagingSenderId: "49685789530",
-  appId: "1:49685789530:web:ca722208b0e7ccf90d9163"
-};
+const signupBtn = document.getElementById("signupBtn");
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+signupBtn.addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("تم إنشاء الحساب بنجاح!");
+    })
+    .catch((error) => {
+      alert("خطأ: " + error.message);
+    });
+});
