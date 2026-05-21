8a43244b13e277d910bcd51d74af6205b884181a
jahniasfnnn-ux <jahniasfnnn@gmail.com>
2026-05-21 16:21:56 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPrIbAxrLNuPINKa4nf0MVMl_Jf32vblA",
+  authDomain: "myshop-9cf11.firebaseapp.com",
+  projectId: "myshop-9cf11",
+  storageBucket: "myshop-9cf11.firebasestorage.app",
+  messagingSenderId: "49685789530",
+  appId: "1:49685789530:web:ca722208b0e7ccf90d9163"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
